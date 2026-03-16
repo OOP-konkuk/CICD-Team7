0cc6551451c75bc8c45a0482fd0bb92e6c350900
강희진 <kanghuijin12@gmail.com>
2026-03-16 22:53:36 +0900

    test: 정적분석 도구 테스트

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
 int main() {
-    printf("테스트");
-    printf("테스트");
-    printf("테스트");
-    printf("테스트");
+    // cppcheck: 초기화 안 된 변수
+    int x;
+    std::cout << x << std::endl;
+
+    // cppcheck: 메모리 누수
+    int* ptr = new int[10];
+
+    // clang-tidy: 사용 안 하는 변수
+    int unused = 42;
+
+    // cppcheck: 배열 범위 초과
+    int arr[5];
+    arr[10] = 1;
+
     return 0;
 }
